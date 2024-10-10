91efeec8fa9308a92f6f289df57a4ea794daf84c
avneets2103 <avneets2103@gmail.com>
2024-10-10 22:27:53 +0530

    vercel issue

--- frontend/Helpers/cookieHandling.ts
+++ frontend/Helpers/cookieHandling.ts
@@ -1,7 +1,1 @@
-import { accessTokenExpiration, refreshTokenExpiration } from "@/CONSTANTS";
-import Cookies from "js-cookie";
 
-export const tokenCookies = (accessToken: string, refreshToken: string) => {
-    Cookies.set("accessToken", accessToken, {expires: accessTokenExpiration});
-    Cookies.set("refreshToken", refreshToken, {expires: refreshTokenExpiration});
-}
