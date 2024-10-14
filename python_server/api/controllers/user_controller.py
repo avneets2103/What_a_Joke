493592537e82b73682b77bcd730e2dc7430aad60
avneets2103 <avneets2103@gmail.com>
2024-10-14 16:42:17 +0530

    humor model added

--- python_server/api/controllers/user_controller.py
+++ python_server/api/controllers/user_controller.py
@@ -7,17 +7,23 @@ import numpy as np
 import math
 from gensim.models import Word2Vec
 import os
-# from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-# with open('path/to/humor_model.pkl', 'rb') as humor_model_file:
-#     humor_model = pickle.load(humor_model_file)
-
+# offense models
 offense_model_path = os.path.join(os.path.dirname(__file__), '../ai_models/offense_model.pkl')
 with open(offense_model_path, 'rb') as f:
     offense_model = pickle.load(f)
 
 offense_w2v_path = os.path.join(os.path.dirname(__file__), '../ai_models/offense_w2v.model')
 w2v_model_offense = Word2Vec.load(offense_w2v_path)
+
+# humor models
+humor_model_path = os.path.join(os.path.dirname(__file__), '../ai_models/humor_model.pkl')
+with open(humor_model_path, 'rb') as f:
+    humor_model = pickle.load(f)
+
+humor_w2v_path = os.path.join(os.path.dirname(__file__), '../ai_models/humor_w2v.model')
+w2v_model_humor = Word2Vec.load(humor_w2v_path)
 
 slang_dict = {
     "AFAIK": "As Far As I Know",
@@ -154,25 +160,70 @@ def offense_preprocess_joke(joke):
     
     return input
 
+def humor_preprocess_joke(joke):
+    # Replace slangs using the dictionary
+    for slang, replacement in slang_dict.items():
+        joke = joke.replace(slang, replacement)
+    
+    # Convert to lowercase
+    joke = joke.lower()
+    
+    # Convert emojis and remove colons
+    def convert_emojis_and_remove_colons(text):
+        text_with_emojis = emoji.demojize(text)
+        return re.sub(r':(\w+):', r'\1', text_with_emojis)
+    
+    joke = convert_emojis_and_remove_colons(joke)
+    
+    # Text correction
+    blob = tb.TextBlob(joke)
+    joke = str(blob.correct())
+    
+    # Tokenize using spaCy
+    def tokenize_text(text):
+        if isinstance(text, str):
+            doc = nlp(text) 
+            return [token.text for token in doc]
+        else:
+            return []
+    tokens = tokenize_text(joke)
+    
+    # w2v
+    def average_vector(tokens, model):
+        vectors = []
+        for token in tokens:
+            if token in model.wv:
+                vectors.append(model.wv[token])
+        # If no vectors are found, return a zero vector
+        if not vectors:
+            return np.zeros(model.vector_size)
+        # Calculate the average
+        return np.mean(vectors, axis=0)
+    input = average_vector(tokens, w2v_model_humor)
+    input = input.reshape(-1, 1)  # Reshape to (100, 1)
+    input = input.reshape(1, 1, input.shape[0])  # Reshape to (1, 1, 100)
+    
+    return input
+
 def joke_analyse(request):
     data = request.get_json()
     joke = data.get('joke')
-
-    # Preprocess the joke
-    # processed_joke = preprocess_joke(joke)
-
-    # Predict humor score
-    # humorScore = humor_model.predict([processed_joke])[0]  # Assuming your model requires a list
 
     # offense processing
     offense_processed_joke = offense_preprocess_joke(joke)
 
     # Predict offense score
-    offenseScore = math.ceil((offense_model.predict(offense_processed_joke))*100) # Same assumption as above
+    offenseScore = math.ceil((offense_model.predict(offense_processed_joke))*100)
+    
+    # Preprocess the joke
+    humor_processed_joke = humor_preprocess_joke(joke)
+
+    # Predict humor score
+    humorScore = max(0, min(100, math.ceil((humor_model.predict(humor_processed_joke)))))
 
     return {
         "message": "Joke analysis done!",
-        "humorScore": 0,
+        "humorScore": humorScore,
         "offenseScore": offenseScore,
         "joke": joke
     }, 201
