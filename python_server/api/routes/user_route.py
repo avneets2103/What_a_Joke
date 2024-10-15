38928ae2357a92556835dd9127197fa65e3dad3e
Avneet singh <93909103+avneets2103@users.noreply.github.com>
2024-10-15 20:44:59 +0530

    Delete python_server directory

--- python_server/api/routes/user_route.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, request
-from controllers.user_controller import joke_analyse
-
-user_bp = Blueprint('user_bp', __name__)
-
-@user_bp.route('/jokeAnalysis', methods=['POST'])
-def register():
-    return joke_analyse(request)
